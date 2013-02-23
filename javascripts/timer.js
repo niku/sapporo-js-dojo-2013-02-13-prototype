ea9ab367830dfef0780e880bc515c6e5096df7fb
niku <niku@niku.name>
2013-02-23 17:07:34 +0900

    fix indent

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,14 +1,14 @@
 $(function() {
-var remain = 5;
-var timer = setInterval(function() {
-  remain = remain - 1;
-  if(remain < 0) {
-    $.post('/pomodoro', { done: "hogehoge" }, function (data) {
-      $('#doneList').text(data.join(','));
-    });
-    clearInterval(timer);
-    return;
-  }
-  document.getElementById('timer').innerHTML = remain;
-}, 1000);
+  var remain = 5;
+  var timer = setInterval(function() {
+    remain = remain - 1;
+    if(remain < 0) {
+      $.post('/pomodoro', { done: "hogehoge" }, function (data) {
+        $('#doneList').text(data.join(','));
+      });
+      clearInterval(timer);
+      return;
+    }
+    document.getElementById('timer').innerHTML = remain;
+  }, 1000);
 });
