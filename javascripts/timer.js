7764cae238f4261005bcb4446afe85f3e3303d70
niku <niku@niku.name>
2013-02-23 15:43:03 +0900

    add timer

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,1 +1,9 @@
-alert('called!');
+var remain = 25;
+var timer = setInterval(function() {
+  remain = remain - 1;
+  if(remain < 0) {
+    clearInterval(timer);
+    return;
+  }
+  document.getElementById('timer').innerHTML = remain;
+}, 1000);
