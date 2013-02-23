5fb099c9ee654f4a109746a8c61870ac28b4bfba
niku <niku@niku.name>
2013-02-23 17:18:38 +0900

    take task name

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,10 +1,11 @@
 $(function() {
   var remain = 5;
   $('#start-button').click(function() {
+    var task = $('#task').val();
     var timer = setInterval(function() {
       remain = remain - 1;
       if(remain < 0) {
-        $.post('/pomodoro', { done: "hogehoge" }, function (data) {
+        $.post('/pomodoro', { done: task }, function (data) {
           $('#doneList').text(data.join(','));
         });
         clearInterval(timer);
