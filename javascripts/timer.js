cdfa0e089b60c241d3256fe5c0607c27c72f1122
niku <niku@niku.name>
2013-02-23 17:12:23 +0900

    start timer when butten clicked

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,5 +1,6 @@
 $(function() {
   var remain = 5;
+  $('#start-button').click(function() {
   var timer = setInterval(function() {
     remain = remain - 1;
     if(remain < 0) {
@@ -11,4 +12,5 @@ $(function() {
     }
     document.getElementById('timer').innerHTML = remain;
   }, 1000);
+  });
 });
